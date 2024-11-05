ef9018ec9450d3075c78457aa32c1ed4a1d4aae9
Raven-o <karendayananino@gmail.com>
2024-11-04 23:32:14 -0500

    Clase Principal, Pokémon, Entrenador,EnumTipoPokemon
    Principal: Menú Principal y Submenú Uno.
    Pokémon:  método entrenar.
    Entrenador: Clase y constructor.
    TipoPokemon : creación de enum.

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -2,8 +2,24 @@ package simulador.pokemon;
 
 public abstract class Pokemon {
     private String nombre;
-    private double salud;
+    private int salud;
     private int puntosDeAtaque;
     private String tipo;
     private String estado;
+
+    public Pokemon(String nombre, int salud, int puntosDeAtaque, String tipo) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void entrenar(){
+        this.salud =+2;
+    }
 }
+
