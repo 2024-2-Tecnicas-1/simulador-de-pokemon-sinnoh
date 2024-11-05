45c931a09826e8995b26465c7e09a63b566cb75e
Raven-o <karendayananino@gmail.com>
2024-11-04 21:54:58 -0500

    Clase Principal y Pokémon
    Principal: creación de submenús.
    Pokémon: Constructor y getNombre

--- src/main/java/simulador/Principal.java
+++ src/main/java/simulador/Principal.java
@@ -3,5 +3,12 @@ package simulador;
 public class Principal {
     public static void main(String[] args) {
         // TODO: Aquí va tu código
+        
+        System.out.println("Simulador de Batallas Pokémon");
+        System.out.println("1. Gestionar Entrenadores");
+        System.out.println("2. Gestionar Pokémones");
+        System.out.println("3. Iniciar Batalla");
+        System.out.println("4. Salir");
+        
     }
 }
--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -2,8 +2,9 @@ package simulador.pokemon;
 
 public abstract class Pokemon {
     private String nombre;
-    private double salud;
+    private int salud;
     private int puntosDeAtaque;
     private String tipo;
     private String estado;
 }
+
